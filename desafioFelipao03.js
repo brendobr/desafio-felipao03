6830bbd3f0403bf660b872af7772f5eabcb4f626
Brendo Araújo <152037647+brendobr@users.noreply.github.com>
2023-12-04 21:54:50 -0300

    Update desafioFelipao03.js

--- desafioFelipao03.js
+++ desafioFelipao03.js
@@ -1,10 +1,11 @@
+//Aqui declaramos a "class" contendo os atributos do Heroi
 class Heroi {
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
-
+// Estrutura de decisão
     ataque() {
         let tipoAtaque;
 
@@ -17,13 +18,13 @@ class Heroi {
         } else if (this.tipo === "ninja") {
             tipoAtaque = "shuriken";
         }
-
+// mensagem printando o tipo do heroi e o tipo de ataque utilizado
         console.log(`O tipo ${this.tipo} usou ${tipoAtaque}`);
     }
 }
-
+// Array  contendo os tipo de Heroi
 let tiposHerois = ["guerreiro", "mago", "monge", "ninja"];
-
+// Loop para decidir o tipo do heroi
 for (let i = 0; i < tiposHerois.length; i++) {
     let heroi = new Heroi("Valentim", 7, tiposHerois[i]);
     heroi.ataque();
